b29e8d2e6a95e3de0db2644ea5632de1d480edea
JesusTod <buchenwald.dunkelheit@gmail.com>
2022-10-22 14:16:15 +0300

    Base Entities

---
+++ kinolog/DAL/Entities/Gender.cs
@@ -0,0 +1,9 @@
+﻿namespace DAL.Entities
+{
+    public class Gender
+    {
+        public int Id { get; set; }
+        public string? Name{ get; set; }
+        public IEnumerable<User>? Users{ get; set; }
+    }
+}
