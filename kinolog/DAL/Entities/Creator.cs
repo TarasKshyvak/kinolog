ab828ad88bc0eebeef38d7612309ede399e1d3e2
TarasKshyvak <76743055+TarasKshyvak@users.noreply.github.com>
2022-10-25 10:50:50 +0300

    correct misspell

--- kinolog/DAL/Entities/Creator.cs
+++ kinolog/DAL/Entities/Creator.cs
@@ -4,7 +4,7 @@
     {
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public Guid CountyId { get; set; }
+        public Guid CountryId { get; set; }
         public IEnumerable<Movie>? Movies { get; set; }
         public Country? Country { get; set; }
     }
