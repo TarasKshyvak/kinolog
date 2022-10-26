11dd5fcd0e9f5a57c3f2de8ccae74e8289b97313
JesusTod <61059120+JesusTod@users.noreply.github.com>
2022-10-26 11:03:03 +0300

    Repositories (#8)

--- kinolog/DAL/Repositories/CountryRepository.cs
+++ kinolog/DAL/Repositories/CountryRepository.cs
@@ -1,14 +1,48 @@
-﻿using DAL.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL.Data;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Repositories
 {
     public class CountryRepository : ICountryRepository
     {
+        private readonly KinologDbContext _context;
+        public CountryRepository(KinologDbContext kinologDbContext)
+        {
+            _context = kinologDbContext;
+        }
+
+
+        public async Task<IEnumerable<Country>> GetAllAsync()
+        {
+            return await _context.Countries.ToListAsync();
+        }
+
+        public async Task<Country> GetByIdAsync(Guid id)
+        {
+            return await _context.Countries.FindAsync(id);
+        }
+        public async Task AddAsync(Country country)
+        {
+            await _context.AddAsync(country);
+        }
+
+        public void Delete(Country country)
+        {
+            _context.Countries.Remove(country);
+        }
+
+        public async Task DeleteByIdAsync(Guid id)
+        {
+            var c = await GetByIdAsync(id);
+            _context.Countries.Remove(c);
+        }
+
+        public void Update(Country country)
+        {
+            _context.Countries.Update(country);
+        }
 
     }
 }
--- kinolog/DAL/Repositories/MovieRepository.cs
+++ kinolog/DAL/Repositories/MovieRepository.cs
@@ -1,14 +1,48 @@
-﻿using DAL.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL.Data;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Repositories
 {
     public class MovieRepository : IMovieRepository
     {
+        private readonly KinologDbContext _context;
+        public MovieRepository(KinologDbContext kinologDbContext)
+        {
+            _context = kinologDbContext;
+        }
+
+
+        public async Task<IEnumerable<Movie>> GetAllAsync()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+
+        public async Task<Movie> GetByIdAsync(Guid id)
+        {
+            return await _context.Movies.FindAsync(id);
+        }
+        public async Task AddAsync(Movie movie)
+        {
+            await _context.AddAsync(movie);
+        }
+
+        public void Delete(Movie movie)
+        {
+            _context.Movies.Remove(movie);
+        }
+
+        public async Task DeleteByIdAsync(Guid id)
+        {
+            var m = await GetByIdAsync(id);
+            _context.Movies.Remove(m);
+        }
+
+        public void Update(Movie movie)
+        {
+            _context.Movies.Update(movie);
+        }
 
     }
 }
