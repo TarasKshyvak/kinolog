8b34578a3570ad7dc1525c9f1d95e542ddde53a6
JesusTod <buchenwald.dunkelheit@gmail.com>
2022-10-26 10:58:35 +0300

    Repositories
    Repo

--- kinolog/DAL/Repositories/CreatorRepository.cs
+++ kinolog/DAL/Repositories/CreatorRepository.cs
@@ -1,14 +1,48 @@
-﻿using DAL.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL.Data;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Repositories
 {
     public class CreatorRepository : ICreatorRepository
     {
+        private readonly KinologDbContext _context;
+        public CreatorRepository(KinologDbContext kinologDbContext)
+        {
+            _context = kinologDbContext;
+        }
+
+
+        public async Task<IEnumerable<Creator>> GetAllAsync()
+        {
+            return await _context.Creators.ToListAsync();
+        }
+
+        public async Task<Creator> GetByIdAsync(Guid id)
+        {
+            return await _context.Creators.FindAsync(id);
+        }
+        public async Task AddAsync(Creator creator)
+        {
+            await _context.AddAsync(creator);
+        }
+
+        public void Delete(Creator creator)
+        {
+            _context.Creators.Remove(creator);
+        }
+
+        public async Task DeleteByIdAsync(Guid id)
+        {
+            var c = await GetByIdAsync(id);
+            _context.Creators.Remove(c);
+        }
+
+        public void Update(Creator creator)
+        {
+            _context.Creators.Update(creator);
+        }
 
     }
 }
--- kinolog/DAL/Repositories/UserRepository.cs
+++ kinolog/DAL/Repositories/UserRepository.cs
@@ -1,13 +1,48 @@
-﻿using DAL.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL.Data;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Repositories
 {
     public class UserRepository : IUserRepository
     {
+        private readonly KinologDbContext _context;
+        public UserRepository(KinologDbContext kinologDbContext)
+        {
+            _context = kinologDbContext;
+        }
+
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetByIdAsync(Guid id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+        public async Task AddAsync(User user)
+        {
+            await _context.AddAsync(user);
+        }
+
+        public void Delete(User user)
+        {
+            _context.Users.Remove(user);
+        }
+
+        public async Task DeleteByIdAsync(Guid id)
+        {
+            var u = await GetByIdAsync(id);
+            _context.Users.Remove(u);
+        }
+
+        public void Update(User user)
+        {
+            _context.Users.Update(user);
+        }
+
     }
 }
