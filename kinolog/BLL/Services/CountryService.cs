aa98855778223c8e363b748f72a101f707aff07a
JesusTod <buchenwald.dunkelheit@gmail.com>
2022-10-31 11:15:46 +0200

    BL services

--- kinolog/BLL/Services/CountryService.cs
+++ kinolog/BLL/Services/CountryService.cs
@@ -1,33 +1,59 @@
-﻿using BLL.Interfaces;
+﻿using AutoMapper;
+using BLL.Interfaces;
 using BLL.Models;
+using DAL.Data;
+using DAL.Entities;
+using DAL.Repositories;
 
 namespace BLL.Services
 {
     public class CountryService : ICountryService
     {
-        public Task AddAsync(CountryModel model)
+        private readonly IMapper _mapper;
+        private readonly CountryRepository _countryRepository;
+
+
+        public CountryService(KinologDbContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            _countryRepository = new CountryRepository(context);
         }
 
-        public Task DeleteAsync(Guid modelId)
+        public async Task AddAsync(CountryModel model)
         {
-            throw new NotImplementedException();
+            ArgumentNullException.ThrowIfNull(model);
+            var entity = _mapper.Map<Country>(model);
+
+            await _countryRepository.AddAsync(entity);
+            await _countryRepository.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<CountryModel>> GetAllAsync()
+        public async Task DeleteAsync(Guid modelId)
         {
-            throw new NotImplementedException();
-        }
-        
-        public Task<CountryModel> GetByIdAsync(Guid id)
-        {
-            throw new NotImplementedException();
+            var entity = await _countryRepository.GetByIdAsync(modelId);
+            ArgumentNullException.ThrowIfNull(entity);
+
+            await _countryRepository.DeleteByIdAsync(modelId);
+            await _countryRepository.SaveChangesAsync();
         }
 
-        public Task UpdateAsync(CountryModel model)
+        public async Task<IEnumerable<CountryModel>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return _mapper.Map<IEnumerable<CountryModel>>(await _countryRepository.GetAllAsync());
+        }
+
+        public async Task<CountryModel> GetByIdAsync(Guid id)
+        {
+            return _mapper.Map<CountryModel>(await _countryRepository.GetByIdAsync(id));
+        }
+
+        public async Task UpdateAsync(CountryModel model)
+        {
+            ArgumentNullException.ThrowIfNull(model);
+
+            var entity = await _countryRepository.GetByIdAsync(model.Id);
+            _countryRepository.Update(entity);
+            await _countryRepository.SaveChangesAsync();
         }
     }
 }
