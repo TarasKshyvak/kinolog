0edab8cf1e5e478376e3e6168555c227f70b31a9
TarasKshyvak <76743055+TarasKshyvak@users.noreply.github.com>
2022-11-01 15:53:52 +0200

    Movie models validators (#16)
    * CreatorModelValidator
    
    * Movie models validators

--- kinolog/BLL/Models/MovieCreatorModel.cs
+++ kinolog/BLL/Models/MovieCreatorModel.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public Guid MovieId { get; set; }
         public Guid CreatorId { get; set; }
-        public Guid PositionId { get; set; }
+        public int PositionId { get; set; }
         public string Movie { get; set; } = null!;
         public string Creator { get; set; } = null!;
         public string Position { get; set; } = null!;
--- kinolog/BLL/Validators/MovieNameModelValidator.cs
+++ kinolog/BLL/Validators/MovieNameModelValidator.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BLL.Models;
+using FluentValidation;
 
 namespace BLL.Validators
 {
-    public class MovieNameModelValidator
+    public class MovieNameModelValidator : AbstractValidator<MovieNameModel>
     {
-
+        public MovieNameModelValidator()
+        {
+            RuleFor(movie => movie.Name)
+                .NotEmpty()
+                .WithMessage("Name is required field");
+        }
     }
 }
