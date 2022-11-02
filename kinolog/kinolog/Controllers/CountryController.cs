670c6c24db067693222e8e132367857ca1be5ba3
JesusTod <buchenwald.dunkelheit@gmail.com>
2022-11-02 19:34:28 +0200

    Controllers

---
+++ kinolog/kinolog/Controllers/CountryController.cs
@@ -0,0 +1,53 @@
+﻿using BLL.Interfaces;
+using BLL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kinolog.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class CountryController : ControllerBase
+    {
+        private readonly ICountryService _countryService;
+
+        public CountryController(ICountryService countryService)
+        {
+            _countryService = countryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CountryModel>>> GetAll()
+        {
+            return (await _countryService.GetAllAsync()).ToList();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CountryModel>> GetById(Guid id)
+        {
+            return await _countryService.GetByIdAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add([FromBody] CountryModel model)
+        {
+            await _countryService.AddAsync(model);
+            return Ok(model);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(Guid id, CountryModel model)
+        {
+            model.Id = id;
+            await _countryService.UpdateAsync(model);
+            return Ok(model);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            await _countryService.DeleteAsync(id);
+            return Ok();
+        }
+    }
+}
